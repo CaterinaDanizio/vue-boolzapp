8055a02f380c496c97434994306c0ee070211d9b
Caterina <danizio@outlook.it>
2020-11-23 16:47:44 +0100

    Chat's messages in chat-area

--- js/script.js
+++ js/script.js
@@ -7,38 +7,57 @@
 var app = new Vue({
   el: '#window',
   data: {
-    chat: [
+      chatActive: 0,
+      chat: [
+          {
+          img: "img/avatar_1.jpg",
+          username: "Michele",
+          lastAccess: "Ultimo accesso oggi 20.11.2020 alle 16.43",
+          lastMessage: "",
+// Creo un array che contenga i messaggi della chat con ogni singolo user
+          indexMsg: 0,
+          msg: [
+            {message1: "Ciao come stai?",
+            message2: "Io tutto alla grande, mi sono ripresa completamente. E tu?",
+            message3: "Non c'è male...La serata di ieri è stata una bomba atomica. Peccato che non sei riuscita a venire. Come stai oggi?",
+            message4: "Dai allora la prossima volta non puoi mancare"
+          },
+          ],
+        },
         {
-        img: "img/avatar_1.jpg",
-        username: "Michele",
-        lastAccess: "Ultimo accesso oggi 20.11.2020 alle 16.43",
-        lastMessage: "Dai allora ci vediamo la prossima settimana"
+        img: "img/avatar_2.jpg",
+        username: "Fabio",
+        lastAccess: "Ultimo accesso oggi 21.11.2020 alle 17.43",
+        lastMessage: "Fai una capricciosa, grazie",
+
+        indexMsg: 0,
+        msg: [
+          {message1: "Stasera prendo le pizze",
+          message2: "Ah ok perfetto...ma per che ora facciamo?",
+          message3: "Pensavo per le 21, ok? Dimmi come la vuoi che le ordino in anticipo",
+          message4: "Fai una capricciosa, grazie",
+          message5: "Vaaaa benee, a più tardi"
+        },
+        ],
       },
       {
-      img: "img/avatar_2.jpg",
-      username: "Fabio",
-      lastAccess: "Ultimo accesso oggi 21.11.2020 alle 17.43",
-      lastMessage: "Ok la prossima volta non potrai mancare"
+      img: "img/avatar_3.jpg",
+      username: "Samuele",
+      lastAccess: "Ultimo accesso oggi 22.11.2020 alle 17.43",
+      lastMessage: "Sei la migliore ever"
     },
-    {
-    img: "img/avatar_3.jpg",
-    username: "Samuele",
-    lastAccess: "Ultimo accesso oggi 22.11.2020 alle 17.43",
-    lastMessage: "Sei la migliore ever"
+      {
+      img: "img/avatar_4.jpg",
+      username: "Luisa",
+      lastAccess: "Ultimo accesso oggi 24.11.2020 alle 18.43",
+      lastMessage: "Dove sei?"
+    },
+  ],
+},
+
+methods: {
+     chatSelect: function (index) {
+       this.chatActive = index;
+     },
   },
-    {
-    img: "img/avatar_4.jpg",
-    username: "Luisa",
-    lastAccess: "Ultimo accesso oggi 24.11.2020 alle 18.43",
-    lastMessage: "Dove sei?"
-    },
-
-
-    // {
-    //   img: "img/avatar_2.jpg",
-    //   username: "Fabio",
-    //   lastAccess: "Ultimo accesso oggi 20.11.2020 alle 16.43"
-    // },
-    ]
-  }
 })
