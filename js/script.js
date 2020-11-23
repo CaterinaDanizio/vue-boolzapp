fce3fbac2de084160ce75c4d3a1a9ed8cb891783
Caterina <danizio@outlook.it>
2020-11-23 21:05:59 +0100

    Dynamic class & content with Vue

--- js/script.js
+++ js/script.js
@@ -3,61 +3,141 @@
 // La relativa chat in elenco rimane selezionata
 
 // Istanza Vue js
-
 var app = new Vue({
   el: '#window',
   data: {
+
+    // Index chat attiva
       chatActive: 0,
+
+  // Elenco chat
+    // Chat 1
       chat: [
           {
           img: "img/avatar_1.jpg",
           username: "Michele",
           lastAccess: "Ultimo accesso oggi 20.11.2020 alle 16.43",
-          lastMessage: "",
-// Creo un array che contenga i messaggi della chat con ogni singolo user
-          indexMsg: 0,
+
+    // Chat 1: messaggi
           msg: [
-            {message1: "Ciao come stai?",
-            message2: "Io tutto alla grande, mi sono ripresa completamente. E tu?",
-            message3: "Non c'è male...La serata di ieri è stata una bomba atomica. Peccato che non sei riuscita a venire. Come stai oggi?",
-            message4: "Dai allora la prossima volta non puoi mancare"
-          },
+            {type: "sent",
+            text: "Ciao come stai?"
+            },
+
+            {type: "ric",
+             text: "Io tutto alla grande, mi sono ripresa completamente. E tu?"
+            },
+
+            {type: "sent",
+             text: "Io tutto alla grande, mi sono ripresa completamente. E tu?"
+            },
+
+            {type: "ric",
+             text: "Non c'è male...La serata di ieri è stata una bomba atomica. Peccato che non sei riuscita a venire. Come stai oggi?"
+            },
+
+            {type: "sent",
+             text: "Dai allora la prossima volta non puoi mancare"
+            },
           ],
         },
+
+    // Chat 2
         {
         img: "img/avatar_2.jpg",
         username: "Fabio",
         lastAccess: "Ultimo accesso oggi 21.11.2020 alle 17.43",
-        lastMessage: "Fai una capricciosa, grazie",
 
-        indexMsg: 0,
+
+    // Chat 2: messaggi
         msg: [
-          {message1: "Stasera prendo le pizze",
-          message2: "Ah ok perfetto...ma per che ora facciamo?",
-          message3: "Pensavo per le 21, ok? Dimmi come la vuoi che le ordino in anticipo",
-          message4: "Fai una capricciosa, grazie",
-          message5: "Vaaaa benee, a più tardi"
-        },
+          {type: "sent",
+          text: "Stasera prendo le pizze"
+          },
+
+          {type: "ric",
+           text: "Ah ok perfetto...ma per che ora facciamo?"
+          },
+
+          {type: "sent",
+           text: "Pensavo per le 21, ok? Dimmi come la vuoi che le ordino in anticipo"
+          },
+
+          {type: "ric",
+           text: "Fai una capricciosa, grazie"
+          },
+
+          {type: "sent",
+           text: "Vaaaa benee, a più tardi"
+          },
         ],
-      },
+       },
+
+    // Chat 3
       {
       img: "img/avatar_3.jpg",
       username: "Samuele",
-      lastAccess: "Ultimo accesso oggi 22.11.2020 alle 17.43",
-      lastMessage: "Sei la migliore ever"
-    },
+      lastAccess: "Ultimo accesso oggi 23.11.2020 alle 21.43",
+
+    // Chat 3: messaggi
+      msg: [
+        {type: "ric",
+        text: "Ho preso un cagnolino e ho bisogno di chiederti qualche consiglio "
+        },
+
+        {type: "sent",
+         text: "Noooo davvero? Spettacolo...come si chiama? Dove l'hai preso?"
+        },
+
+        {type: "ric",
+         text: "L'ho chiamata Nala perchè sembra una piccola leonessa. L'ho presa in canile, è una meticcia"
+        },
+
+        {type: "ric",
+         text: "Quando puoi ci sentiamo che ti racconto"
+        },
+
+        {type: "sent",
+         text: "Volentieri"
+        },
+      ],
+      },
+
+      // Chat 4
+
       {
       img: "img/avatar_4.jpg",
       username: "Luisa",
       lastAccess: "Ultimo accesso oggi 24.11.2020 alle 18.43",
-      lastMessage: "Dove sei?"
+
+      msg: [
+        {type: "ric",
+        text: "Ti devo raccontare dell'altra sera"
+        },
+
+        {type: "sent",
+         text: "Cos'è successo? Voglio i dettagli"
+        },
+
+        {type: "ric",
+         text: "Ehehehe sono uscita con un tipo nuovo, affascinante...abbiamo passato una bella serata"
+        },
+
+        {type: "ric",
+         text: "Dopo tanti perdigiorno...finalmente uno decente"
+        },
+
+        {type: "sent",
+         text: "Dobbiamo sentirci assolutamente, devo sapere tutto"
+        },
+      ],
     },
   ],
 },
 
 methods: {
      chatSelect: function (index) {
-       this.chatActive = index;
+       return this.chatActive = index;
      },
   },
 })
