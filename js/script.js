06238a30b67724a89f2c14b2791e0981ac863d8c
Caterina <danizio@outlook.it>
2020-11-24 13:09:58 +0100

    Date's messages

--- js/script.js
+++ js/script.js
@@ -16,28 +16,33 @@ var app = new Vue({
           {
           img: "img/avatar_1.jpg",
           username: "Michele",
-          lastAccess: "Ultimo accesso oggi 20.11.2020 alle 16.43",
+          lastAccess: "Ultimo accesso 20.11.2020 alle 16.43",
 
     // Chat 1: messaggi
           msg: [
             {type: "sent",
-            text: "Ciao come stai?"
+            text: "Ciao come stai?",
+            date: "20.11.2020 ore 16.00"
             },
 
             {type: "ric",
-             text: "Io tutto alla grande, mi sono ripresa completamente. E tu?"
+             text: "Io tutto alla grande, mi sono ripresa completamente. E tu?",
+             date: "20.11.2020 ore 16.03"
             },
 
             {type: "sent",
-             text: "Io tutto alla grande, mi sono ripresa completamente. E tu?"
+             text: "Io tutto alla grande, mi sono ripresa completamente. E tu?",
+             date: "20.11.2020 ore 16.07"
             },
 
             {type: "ric",
-             text: "Non c'è male...La serata di ieri è stata una bomba atomica. Peccato che non sei riuscita a venire. Come stai oggi?"
+             text: "Non c'è male...La serata di ieri è stata una bomba atomica. Peccato che non sei riuscita a venire. Come stai oggi?",
+             date: "20.11.2020 ore 16.09"
             },
 
             {type: "sent",
-             text: "Dai allora la prossima volta non puoi mancare"
+             text: "Dai allora la prossima volta non puoi mancare",
+             date: "20.11.2020 ore 16.11"
             },
           ],
         },
@@ -46,29 +51,34 @@ var app = new Vue({
         {
         img: "img/avatar_2.jpg",
         username: "Fabio",
-        lastAccess: "Ultimo accesso oggi 21.11.2020 alle 17.43",
+        lastAccess: "Ultimo accesso 21.11.2020 alle 17.43",
 
 
     // Chat 2: messaggi
         msg: [
           {type: "sent",
-          text: "Stasera prendo le pizze"
+          text: "Stasera prendo le pizze",
+          date: "21.11.2020 ore 16.03"
           },
 
           {type: "ric",
-           text: "Ah ok perfetto...ma per che ora facciamo?"
+           text: "Ah ok perfetto...ma per che ora facciamo?",
+           date: "21.11.2020 ore 16.04"
           },
 
           {type: "sent",
-           text: "Pensavo per le 21, ok? Dimmi come la vuoi che le ordino in anticipo"
+           text: "Pensavo per le 21, ok? Dimmi come la vuoi che le ordino in anticipo",
+           date: "21.11.2020 ore 16.06"
           },
 
           {type: "ric",
-           text: "Fai una capricciosa, grazie"
+           text: "Fai una capricciosa, grazie",
+           date: "21.11.2020 ore 16.07",
           },
 
           {type: "sent",
-           text: "Vaaaa benee, a più tardi"
+           text: "Vaaaa benee, a più tardi. Non vedo l'ora",
+           date: "21.11.2020 ore 16.09"
           },
         ],
        },
@@ -82,23 +92,28 @@ var app = new Vue({
     // Chat 3: messaggi
       msg: [
         {type: "ric",
-        text: "Ho preso un cagnolino e ho bisogno di chiederti qualche consiglio "
+        text: "Ho preso un cagnolino e ho bisogno di chiederti qualche consiglio",
+        date: "19.11.2020 ore 22.15"
         },
 
         {type: "sent",
-         text: "Noooo davvero? Spettacolo...come si chiama? Dove l'hai preso?"
+         text: "Noooo davvero? Spettacolo...come si chiama? Dove l'hai preso?",
+         date: "19.11.2020 ore 22.17"
         },
 
         {type: "ric",
-         text: "L'ho chiamata Nala perchè sembra una piccola leonessa. L'ho presa in canile, è una meticcia"
+         text: "L'ho chiamata Nala perchè sembra una piccola leonessa. L'ho presa in canile, è una meticcia",
+         date: "19.11.2020 ore 22.25"
         },
 
         {type: "ric",
-         text: "Quando puoi ci sentiamo che ti racconto"
+         text: "Quando puoi ci sentiamo che ti racconto",
+         date: "19.11.2020 ore 22.26"
         },
 
         {type: "sent",
-         text: "Volentieri"
+         text: "Volentieri, ci conto",
+         date: "19.11.2020 ore 22.27"
         },
       ],
       },
@@ -106,29 +121,49 @@ var app = new Vue({
       // Chat 4
 
       {
-      img: "img/avatar_4.jpg",
-      username: "Luisa",
-      lastAccess: "Ultimo accesso oggi 24.11.2020 alle 18.43",
+      img: "img/avatar_6.jpg",
+      username: "Sara",
+      lastAccess: "Ultimo accesso 24.11.2020 alle 18.43",
 
       msg: [
         {type: "ric",
-        text: "Ti devo raccontare dell'altra sera"
+        text: "Ti devo raccontare dell'altra sera",
+        date: "24.11.2020 ore 16.03"
         },
 
         {type: "sent",
-         text: "Cos'è successo? Voglio i dettagli"
+         text: "Cos'è successo? Voglio i dettagli",
+         date: "24.11.2020 ore 16.04"
         },
 
         {type: "ric",
-         text: "Ehehehe sono uscita con un tipo nuovo, affascinante...abbiamo passato una bella serata"
+         text: "Ehehehe sono uscita con un tipo nuovo, affascinante...abbiamo passato una bella serata",
+         date: "24.11.2020 ore 16.10"
         },
 
         {type: "ric",
-         text: "Dopo tanti perdigiorno...finalmente uno decente"
+         text: "Dopo tanti perdigiorno...finalmente uno decente",
+         date: "24.11.2020 ore 16.11"
         },
 
         {type: "sent",
-         text: "Dobbiamo sentirci assolutamente, devo sapere tutto"
+         text: "Dobbiamo sentirci assolutamente, devo sapere tutto",
+         date: "24.11.2020 ore 16.15"
+        },
+
+        {type: "sent",
+         text: "Non sto nella pelleeeee",
+         date: "24.11.2020 ore 16.15"
+        },
+
+        {type: "ric",
+         text: "E io non vedo l'ora di raccontarti tutto. Ti chiamo oggi pomeriggio",
+         date: "24.11.2020 ore 16.20"
+        },
+
+        {type: "sent",
+         text: "Ottimo, dopo le 20 mi trovi a casa",
+         date: "24.11.2020 ore 16.27"
         },
       ],
     },
